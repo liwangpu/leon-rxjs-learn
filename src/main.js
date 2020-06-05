b32690368daf778e4c965cf9d6283ec91127ff74
Leon Pu <puliwang@cxist.com>
2020-06-05 18:02:34 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-const { observable, BehaviorSubject, Subject, of, from, interval, timer, pipe, Observable, asyncScheduler, defer } = require('rxjs');
-const { switchMap, mergeMap, concatMap, map, delay, take, tap, observeOn } = require('rxjs/operators');
+const { observable, BehaviorSubject, Subject, of, concat, defer, from, interval, timer, pipe, Observable, asyncScheduler } = require('rxjs');
+const { switchMap, mergeMap, concatMap, map, delay, take, tap, observeOn, publish, share, multicast, refCount } = require('rxjs/operators');
 const { subscriber } = require('./subscriber');
 
-let obs = of(1);
-obs.subscribe(subscriber('s1'));
+// let obs = of(1);
+// obs.subscribe(subscriber('s1'));
+
+function random() {
+    return Math.random();
+}
+
+// let source = defer(() => of(random()))
+// .pipe(delay(0))
+// .pipe(tap(x => console.log('source', x)));
+const source = concat(
+    defer(() => of(random())),
+    defer(() => of(random())).pipe(delay(1))
+);
+
+let m = source.pipe(multicast(() => new Subject()), refCount());
+
+m.subscribe(subscriber('a'));
+m.subscribe(subscriber('b'));
